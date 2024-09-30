f88e50f223d665125b1ee14c3e58627b4efffed0
truptisatsangi <truptisatsangi18@gmail.com>
2024-09-30 17:47:17 +0530

    feat: cli app in go

---
+++ go-gopher-cli-2/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/truptisatsangi/learning-go-by-examples/go-gopher-cli-2/cmd"
+
+func main() {
+	cmd.Execute()
+}
